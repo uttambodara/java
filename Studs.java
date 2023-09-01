8d5c6c931726c5805b8a1e250bb1edc04d35ecda
hackerd <129719033+uttambodara@users.noreply.github.com>
2023-09-01 21:10:57 +0530

    Add files via upload

---
+++ Studs.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Studs {
+    String name;//instance variable
+    int marks;
+    float percent;
+    public Studs(String name,int marks,float percent)//
+    {
+        this.name=name;
+        this.marks=marks;
+        this.percent=percent;
+    }
+}
